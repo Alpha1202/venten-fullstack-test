cfccbaec07b3b6e6b36d27e8024972e72644f9be
Nzubechukwu <nzubennamani@gmail.com>
2020-01-29 15:24:09 +0100

    Updates

---
+++ precache-manifest.97061d60eda3c672e0e2d24a533dd6cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515d90a66e71e7cf0adade3b6588f702",
+    "url": "/venten-fullstack-test/index.html"
+  },
+  {
+    "revision": "3d8439c8628073e89fca",
+    "url": "/venten-fullstack-test/static/css/main.aa381872.chunk.css"
+  },
+  {
+    "revision": "0b8ab26296e703484515",
+    "url": "/venten-fullstack-test/static/js/2.950e38e5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/venten-fullstack-test/static/js/2.950e38e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d8439c8628073e89fca",
+    "url": "/venten-fullstack-test/static/js/main.e428b6de.chunk.js"
+  },
+  {
+    "revision": "b55d682bf7731eaf1af5",
+    "url": "/venten-fullstack-test/static/js/runtime-main.138ce529.js"
+  },
+  {
+    "revision": "f513ef2cb50631a7d2f31256b2c707c3",
+    "url": "/venten-fullstack-test/static/media/logo.f513ef2c.svg"
+  }
+]);
